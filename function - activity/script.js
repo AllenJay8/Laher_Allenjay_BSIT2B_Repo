9325061352671e276c3226f01ecadf0015b305e1
AllenJay8 <alaher@filamer.edu.ph>
2025-03-23 20:36:36 -0700

    3rd

---
+++ function - activity/script.js
@@ -0,0 +1,28 @@
+function whatShallIWear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } 
+    else if (temp < 70) {
+        console.log("Wear a sweater");
+    } 
+    else {
+        console.log("Wear a t-shirt");
+    }
+}
+
+whatShallIWear(50);
+
+function whatShallIWear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } 
+    else if (temp < 70) {
+        console.log("Wear a sweater");
+    } 
+    else {
+        console.log("Wear a t-shirt");
+    }
+}
+
+whatShallIWear(80);
+
