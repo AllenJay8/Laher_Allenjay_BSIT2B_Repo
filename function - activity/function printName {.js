220880fdb283aa0607e4ff0de717e7ed218ca71b
AllenJay8 <alaher@filamer.edu.ph>
2025-03-26 20:46:21 -0700

    4th commit

---
+++ function - activity/function printName {.js
@@ -0,0 +1,7 @@
+function printName(firstName, lastName) {
+    var fullName = firstName + " " + lastName;
+    return fullName;
+}
+
+let result = printName("Allen", "Laher");
+console.log("My Fullname is " + result + "Stupid");
